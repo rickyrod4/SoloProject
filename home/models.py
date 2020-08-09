36def4d030279de30809a5555ccb2edb234d04d8
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-08-08 23:57:17 -0500

    Login/Registration

--- home/models.py
+++ home/models.py
@@ -2,21 +2,28 @@ from django.db import models
 import bcrypt
 import re
 
+EMAIL_MATCH = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
 class UserManager(models.Manager):
     def validations(self, form_data):
         errors = {}
-        if(len(form_data['first_name'])<1):
+        if len(form_data['first_name']) < 1:
             errors['first_name'] = 'Must enter a first name' 
-        if(len(form_data['last_name'])<1):
+        if len(form_data['last_name']) < 1:
             errors['last_name'] = 'Must enter a last name' 
-        if(len(form_data['email'])<1):
+        if len(form_data['email']) < 1:
             errors['email'] = 'Must enter an email'
-        if(len(form_data['password'])<1):
+        if len(form_data['password']) < 1:
             errors['password'] = 'Must enter an password'
-        if(len(form_data['confirmPassword'])<1):
+        if len(form_data['confirmPassword']) < 1:
             errors['confirmPassword'] = 'Must confirm your password'
-        if(form_data['password'] != form_data['confirmPassword']):
-            errors['confirmPassword'] = 'Passwords do not match'                    
+        if form_data['password'] != form_data['confirmPassword']:
+            errors['confirmPassword'] = 'Passwords do not match'   
+        if not EMAIL_MATCH.match(form_data['email']):
+            errors['email'] = 'Invalid Email'
+        users_with_email = self.filter(email=form_data['email'])
+        if users_with_email:
+            errors['email'] = 'Email Is Already Registered'                 
         return errors
 
     def register(self, form_data):
@@ -27,6 +34,15 @@ class UserManager(models.Manager):
             email = form_data['email'],
             password = hash1
         )
+
+    def authenticate(self, email, password):
+        users_with_emails = self.filter(email = email)
+        if not users_with_emails:
+            return False
+        user = users_with_emails[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+
 
 # Create your models here.
 class User(models.Model):
--- home/views.py
+++ home/views.py
@@ -8,4 +8,27 @@ def index(request):
     return render(request, 'index.html')
 
 def register(request):
+    errors = User.objects.validations(request.POST)
+    if errors:
+        for field, value in errors.items():
+            messages.error(request, value)
+            return redirect('/')
+    User.objects.register(request.POST)
+    return redirect('/dashboard')
+
+
+def login(request):
+    result = User.objects.authenticate(request.POST['email'], request.POST['password'])
+    if result == False:
+        messages.error(request, "Invalid email/password")
+    else:
+        user = User.objects.get(email = request.POST['email'])
+        request.session['user_id'] = user.id
+        return redirect('/dashboard')
     return redirect('/')
+
+def dashboard(request):
+    context = {
+        'user' : User.objects.get(id = request.session['user_id'])
+    }
+    return render(request, 'dashboard.html', context)
